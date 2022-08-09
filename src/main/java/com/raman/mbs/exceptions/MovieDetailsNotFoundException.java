23f1cc06d3d13ef9c10ce2ddd9b5a360fb12d45a
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2022-08-09 15:47:37 +0530

    Create MovieDetailsNotFoundException.java

---
+++ src/main/java/com/raman/mbs/exceptions/MovieDetailsNotFoundException.java
@@ -0,0 +1,12 @@
+package com.raman.mbs.exceptions;
+
+public class MovieDetailsNotFoundException extends Exception {
+	
+
+
+		public MovieDetailsNotFoundException(String message) {
+			super(message);
+		}
+	    
+	
+}
