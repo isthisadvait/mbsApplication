f39ed6574ef9761e0f178226b3a44a90e177a69c
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2022-08-09 15:42:22 +0530

    Create User.java

---
+++ src/main/java/com/raman/mbs/entities/User.java
@@ -0,0 +1,150 @@
+package com.raman.mbs.entities;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class User {
+
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    private int userId;
+
+
+		@Column(name = "first_name", length = 20, nullable = false)
+	    private String firstName;
+
+	    @Column(length = 20)
+	    private String lastName;
+
+	    @Column(length = 20, nullable = false, unique = true)
+	    private String username;
+
+	    @Column(length = 20, nullable = false)
+	    private String password;
+
+	    @Column(nullable = false)
+	    private LocalDateTime dateOfBirth;
+
+	    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = {CascadeType.ALL})
+	    @JsonBackReference
+	    private Set<Booking> bookings;
+
+	    @ElementCollection(fetch = FetchType.EAGER)
+	    @CollectionTable(name = "user_contact_number", joinColumns = @JoinColumn(name = "user_id"))
+	    @Column(name = "mobile_number", nullable = false)
+	    private Set<Integer> phoneNumbers;
+
+	    @ManyToOne
+	    @JoinColumn(name = "user_type_id", nullable = false)
+	    private UserType userType;
+
+	    @ManyToOne
+	    @JoinColumn(name = "language_id", nullable = false)
+	    private Language language;
+	    
+	    public int getUserId() {
+			return userId;
+		}
+
+		public void setUserId(int userId) {
+			this.userId = userId;
+		}
+
+	    public Set<Integer> getPhoneNumbers() {
+	        return phoneNumbers;
+	    }
+
+	    public void setPhoneNumbers(Set<Integer> phoneNumbers) {
+	        this.phoneNumbers = phoneNumbers;
+	    }
+
+	    public UserType getUserType() {
+	        return userType;
+	    }
+
+	    public void setUserType(UserType userType) {
+	        this.userType = userType;
+	    }
+
+	    public Language getLanguage() {
+	        return language;
+	    }
+
+	    public void setLanguage(Language language) {
+	        this.language = language;
+	    }
+
+	    public Set<Booking> getBookings() {
+	        return bookings;
+	    }
+
+	    public void setBookings(Set<Booking> bookings) {
+	        this.bookings = bookings;
+	    }
+
+
+	    public String getFirstName() {
+	        return firstName;
+	    }
+
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	    }
+
+	    public String getLastName() {
+	        return lastName;
+	    }
+
+	    public void setLastName(String lastName) {
+	        this.lastName = lastName;
+	    }
+
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+
+	    public LocalDateTime getDateOfBirth() {
+	        return dateOfBirth;
+	    }
+
+	    public void setDateOfBirth(LocalDateTime dateOfBirth) {
+	        this.dateOfBirth = dateOfBirth;
+	    }
+
+		@Override
+		public String toString() {
+			return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", username="
+					+ username + ", password=" + password + ", dateOfBirth=" + dateOfBirth + ", bookings=" + bookings
+					+ ", phoneNumbers=" + phoneNumbers + ", userType=" + userType + ", language=" + language + "]";
+		}
+
+	    
+}
