aa493610c8661f05eabaca5428cfad671b47529d
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2022-08-09 15:41:31 +0530

    Create Theatre.java

---
+++ src/main/java/com/raman/mbs/entities/Theatre.java
@@ -0,0 +1,67 @@
+package com.raman.mbs.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+public class Theatre {
+
+	@Id
+    @GeneratedValue
+    private int theatreId;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String theatreName;
+
+    @Column(nullable = false)
+    private float ticketPrice = 150.00f;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
+    @JsonManagedReference
+    private City city;
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public int getTheatreId() {
+        return theatreId;
+    }
+
+    public void setTheatreId(int theatreId) {
+        this.theatreId = theatreId;
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+    public void setTheatreName(String theatreName) {
+        this.theatreName = theatreName;
+    }
+
+    public float getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(float ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Theatre{" + "theatreId=" + theatreId + ", theatreName='" + theatreName + '\'' + ", ticketPrice="
+                + ticketPrice + '}';
+    }
+}
