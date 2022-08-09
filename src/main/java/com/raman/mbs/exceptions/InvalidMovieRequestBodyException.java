c12f3487701a93563520de739e1de7f9fc09bc2a
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2022-08-09 15:49:18 +0530

    Create InvalidMovieRequestBodyException.java

---
+++ src/main/java/com/raman/mbs/exceptions/InvalidMovieRequestBodyException.java
@@ -0,0 +1,9 @@
+package com.raman.mbs.exceptions;
+
+public class InvalidMovieRequestBodyException extends Exception {
+	
+	public InvalidMovieRequestBodyException(String message) {
+		super(message);
+	}
+
+}
