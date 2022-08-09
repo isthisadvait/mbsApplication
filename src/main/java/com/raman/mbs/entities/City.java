934ede227b93945712316eaaa7127b476205ae0c
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2022-08-09 15:37:51 +0530

    Create City.java

---
+++ src/main/java/com/raman/mbs/entities/City.java
@@ -0,0 +1,68 @@
+package com.raman.mbs.entities;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class City {
+
+	@Id
+    @GeneratedValue
+    private int cityId;
+
+    @Column(length = 20, nullable = false)
+    private String cityName;
+
+    @OneToMany(mappedBy = "city", fetch = FetchType.EAGER)
+    @JsonBackReference
+    private Set<Theatre> theatres;
+
+    public City() {}
+
+
+    public City(int cityId, String cityName) {
+        this.cityId = cityId;
+        this.cityName = cityName;
+    }
+
+    public City(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public Set<Theatre> getTheatres() {
+        return theatres;
+    }
+
+    public void setTheatres(Set<Theatre> theatres) {
+        this.theatres = theatres;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" + "cityId=" + cityId + ", cityName='" + cityName + '\'' + '}';
+    }
+}
