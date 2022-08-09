815ff96bb72df1801189efedeb27b5bd90910807
Raman Mehta <86768225+isthisadvait@users.noreply.github.com>
2022-08-09 15:48:29 +0530

    Create StatusDetailsNotFoundException.java

---
+++ src/main/java/com/raman/mbs/exceptions/StatusDetailsNotFoundException.java
@@ -0,0 +1,9 @@
+package com.raman.mbs.exceptions;
+
+public class StatusDetailsNotFoundException extends Exception {
+
+	public StatusDetailsNotFoundException(String message) {
+		super(message);
+	}
+    
+}
